1735724ac3328567b5e28ecfc0258cde6961c3ed
bazhenak <bazhena.kutergina@gmail.com>
2024-06-13 01:16:18 +0300

    hook useCallback

--- src/Hooks/Hooks.js
+++ src/Hooks/Hooks.js
@@ -1,21 +1,31 @@
-import React, {useCallback, useMemo, useState} from "react";
+import React, {useCallback, useMemo, useRef, useState} from "react";
 
 const UseHooks = () => {
     const [counter, setCounter] = useState(0);
     const [isOpened, setIsOpened] = useState(false);
 
-    const handleClick = useCallback(() => {
-        setCounter(() => counter + 1);
-    }, [counter]);
+    const inputRef = useRef(null);
+
+    const handleFocusInput = () => {
+        ref.current.focus();
+    };
+
+    // const handleClick = useCallback(() => {
+    //     setCounter(() => counter + 1);
+    // }, [counter]);
+
+
 
     return (
         <div>
             <p>Страница для работы с хуками оптимизации</p>
             <div>
+                <input type='text' ref={inputRef}/>
+                <button onClick={handleFocusInput}>Кнопка инпута UseRef</button>
                 <p>Счетчик - {counter}</p>
-                <button onClick={handleClick}>
-                    Увеличить счетчик
-                </button>
+                {/*<button onClick={handleClick}>*/}
+                {/*    Увеличить счетчик*/}
+                {/*</button>*/}
                 <button onClick={() => setIsOpened(!isOpened)}>
                     {isOpened ? 'Закрыть' : 'Открыть'}
                 </button>
