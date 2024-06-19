f228cf9a4318f18129e708f96899a21bae0d822f
bazhenak <bazhena.kutergina@gmail.com>
2024-06-19 10:19:40 +0300

    list rendering

--- src/Pokemons/Pokemons.js
+++ src/Pokemons/Pokemons.js
@@ -1,7 +1,11 @@
 import React, {useEffect, useState} from "react";
-
+import "./Pokemons.css";
 const Pokemons = () => {
     const [pokemons, setPokemons] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(true);
+    const [newElement, setNewElement] = useState({name: "", url: "info"});
+
+    // useEffect(() => console.log(newElement, 'useEf newEl'));
 
     useEffect(() => {
         fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
@@ -9,14 +13,48 @@ const Pokemons = () => {
             .then((res) => {
                 console.log(res.results);
                 setPokemons([...res.results]);
+                setIsLoaded(true);
             });
-    }, []);
+    }, [isLoaded]);
+
+    const deletePokemon = (pokemonName) => {
+        const localPokemons = [...pokemons].filter((el) => el.name !== pokemonName)
+        setPokemons(localPokemons);
+    };
+
+    const addElement = (e, newEl) => {
+        e.preventDefault();
+        const localPokemons = [...pokemons, newEl];
+        setPokemons(localPokemons);
+    };
+
+    const onChangeNewEl = (value, type) => {
+        const newLocalEl = {...newElement};
+        newElement[type] = value;
+        setNewElement(newLocalEl);
+    };
 
     return (
         <div>
+            <button onClick={() => setIsLoaded(false)}>Обновить страницу</button>
+            <div>
+                <form className="poke-form">
+                    <input type="text"
+                           placeholder="name"
+                           onChange={(e) => onChangeNewEl(e.target.value, 'name')}/>
+                    <input type="text"
+                           placeholder="info"
+                           disabled={true}
+                           onChange={(e) => onChangeNewEl(e.target.value, 'url')}/>
+                    <button onClick={(e) => addElement(e, newElement)}>
+                        Добавить элемент
+                    </button>
+                </form>
+            </div>
             {pokemons.map((el, i) => {
                 return (
                 <div className="pokemons-item" key={i}>
+                    <button onClick={() => deletePokemon(el.name)}>Удалить</button>
                     <p className='pokemon-item__name'>{el.name}</p>
                     <a className='pokemon-item__img' target="blank" href={el.url}>
                         {el.name} info
